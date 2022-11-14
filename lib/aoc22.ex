40ec3242b6e0080e38cd32d206940cf1173d86ee
norrbacka <johannes.norrbacka@htomail.com>
2022-11-14 20:58:08 +0100

    Initial hello world...

---
+++ lib/aoc22.ex
@@ -0,0 +1,18 @@
+defmodule Aoc22 do
+  @moduledoc """
+  Documentation for `Aoc22`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc22.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc22_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc22Test do
+  use ExUnit.Case
+  doctest Aoc22
+
+  test "greets the world" do
+    assert Aoc22.hello() == :world
+  end
+end
