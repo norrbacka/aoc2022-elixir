ab054838bc44ee14ed95973da5bd594f99961cd0
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2022-12-02 22:08:12 +0000

    Bad solutions, but hey 2016 was haaard

--- lib/solutions/Solutions.Year2016.Day01.ex
+++ lib/solutions/Solutions.Year2016.Day01.ex
@@ -12,35 +12,59 @@ defmodule Solutions.Year2016.Day01 do
   def rotate(:south, "L"), do: :east
   def rotate(:west, "L"), do: :south
 
-  def traverse(inputs, pos) do
+  def traverse(inputs, pos, all_pos \\ []) do
     if Enum.empty?(inputs) do
-      pos
+      [pos, all_pos]
     else
       [%{dir: dir, steps: steps} | inputs] = inputs
       new_dir = rotate(pos.dir, dir)
-      x = case new_dir do
-        :west -> pos.x - steps
-        :east -> pos.x + steps
-        _ -> pos.x
-      end
-      y = case new_dir do
-        :north -> pos.y - steps
-        :south -> pos.y + steps
-        _ -> pos.y
-      end
+
+      x =
+        case new_dir do
+          :west -> pos.x - steps
+          :east -> pos.x + steps
+          _ -> pos.x
+        end
+
+      y =
+        case new_dir do
+          :north -> pos.y - steps
+          :south -> pos.y + steps
+          _ -> pos.y
+        end
+
+      history =
+        case new_dir do
+          :west ->
+            range = Enum.to_list((pos.x - 1)..x)
+            Enum.map(range, fn i -> [i, pos.y] end)
+
+          :east ->
+            range = Enum.to_list((pos.x + 1)..x)
+            Enum.map(range, fn i -> [i, pos.y] end)
+
+          :north ->
+            range = Enum.to_list((pos.y - 1)..y)
+            Enum.map(range, fn i -> [pos.x, i] end)
+
+          :south ->
+            range = Enum.to_list((pos.y + 1)..y)
+            Enum.map(range, fn i -> [pos.x, i] end)
+        end
+
       pos = %{
         x: x,
         y: y,
         dir: new_dir,
-        history: pos.history ++ [steps]
+        history: pos.history ++ history
       }
-      traverse(inputs, pos)
+
+      traverse(inputs, pos, all_pos ++ [pos])
     end
   end
 
   defmacro __using__(_opts) do
     quote do
-
       def solve(input, 16_01_01) do
         pos = %{x: 0, y: 0, dir: :north, history: []}
 
@@ -52,12 +76,34 @@ defmodule Solutions.Year2016.Day01 do
             steps = String.to_integer(String.trim(steps))
             %{dir: dir, steps: steps}
           end)
-        hq = Solutions.Year2016.Day01.traverse(inputs, pos)
+
+        [hq, _] = Solutions.Year2016.Day01.traverse(inputs, pos)
         abs(hq.x) + abs(hq.y)
       end
 
       def solve(input, 16_01_02) do
-        input
+        pos = %{x: 0, y: 0, dir: :north, history: []}
+
+        inputs =
+          input
+          |> String.split(", ")
+          |> Enum.map(fn x -> String.split_at(x, 1) end)
+          |> Enum.map(fn {dir, steps} ->
+            steps = String.to_integer(String.trim(steps))
+            %{dir: dir, steps: steps}
+          end)
+
+        [hq, allpos] = Solutions.Year2016.Day01.traverse(inputs, pos)
+        history = hq.history |> Enum.map(fn [x, y] -> %{x: x, y: y} end)
+
+        index =
+          Enum.map(history, fn x ->
+            %{cords: x, dupe: Enum.count(history, fn y -> y == x end) == 2}
+          end)
+
+        index
+        |> Enum.find(fn x -> x.dupe end)
+        |> then(fn %{cords: cords, dupe: dupe} -> abs(cords.x) + abs(cords.y) end)
       end
     end
   end
